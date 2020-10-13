949d9647408ca09cd56d2a237b7312d5db2383db
Javier <javierperez@javiers-mbp.home>
2020-10-13 00:13:40 -0400

    first coommit some bugs

---
+++ develop/script.js
@@ -0,0 +1,81 @@
+var textArea = $('textarea')
+var timeBlock = $('.time-block');
+var dailyPlanner = new Array(9);
+
+var StoredArray = JSON.parse(localStorage.getItem('plansToday'));
+var today = moment().format('H');
+console.log(typeof today);
+var calendarHour;
+
+console.log(today);
+
+function getStored() {
+    if (Array.isArray(StoredArray)) {
+        dailyPlanner = JSON.parse(localStorage.getItem('plansToday'));
+    } else {
+        return;
+    }
+}
+getStored();
+console.log(dailyPlanner);
+
+function getContent() {
+    for (var plan = 0; plan < dailyPlanner.length; plan++) {
+        $(`[data-index =${plan}]`).text(dailyPlanner[plan]);
+        console.log($(`[data-index =${plan}]`));
+
+    }
+}
+
+
+
+function getInfo() {
+    for (var i = 9; i <= 17; i++) {
+        var index = i - 9;
+        var row = $('<div>').attr('class', 'row time-block');
+        var timeEl = $('<div>').attr('class', 'col-2  hour');
+
+        var textEl = $('<textarea>').attr('class', 'col-8 textarea description').attr('data-index', index).text('hello')
+        var saveBtn = $('<div>').attr('class', 'col-2 saveBtn').append('<i class="fas fa-save fa-2x"></i>');
+        var hourEl;
+    
+        if (i < 12) {
+            hourEl = `${i}am`;
+            timeEl.attr('data-time', i);
+            console.log(timeEl);
+        } else if (i === 12) {
+            hourEl = `${i} pm`
+            timeEl.attr('data-time', i)
+        } else {
+            hourEl = `${i - 12}pm`
+            timeEl.attr('data-time', i)
+        }
+
+        if (parseInt(timeEl.attr('data-time')) < parseInt(today)) {
+            textEl.addClass('past');
+        } else if (parseInt(timeEl.attr('data-time')) > parseInt(today)) {
+            textEl.addClass('future')
+        } else if (parseInt(timeEl.attr('data-time')) == parseInt(today)) {
+            timeEl.addClass('present')
+        }
+        timeEl.append(hourEl)
+        row.append(timeEl).append(textEl).append(saveBtn);
+        $('.container').append(row)
+
+        // localStorage.getItem(`${dailyPlanner[i].hour}`)
+
+    }
+    getContent();
+}
+getInfo();
+
+$('.saveBtn').on('click', function (e) {
+    var valueEl = $(this).siblings('textarea').val();
+    console.log(valueEl);
+    //attr(data-index) // hour  
+    newPlanIndex = $(this).siblings('textarea').data('index');
+    dailyPlanner.splice(newPlanIndex, 1, valueEl);
+    console.log(dailyPlanner);
+
+    localStorage.setItem(`plansToday`, JSON.stringify(dailyPlanner));
+});
